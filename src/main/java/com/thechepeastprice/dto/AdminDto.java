74eb8722f5c7af984fbcb411ee2ba2913cda2bc2
acakojic <a.kojic@yahoo.com>
2021-04-05 15:54:41 +0200

    removed code that is not required

--- src/main/java/com/thechepeastprice/dto/AdminDto.java
+++ src/main/java/com/thechepeastprice/dto/AdminDto.java
@@ -1,9 +1,7 @@
 package com.thechepeastprice.dto;
 
 import io.swagger.annotations.ApiModel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * Base data transfer object for admin model which groups its id, username, password, and version.
